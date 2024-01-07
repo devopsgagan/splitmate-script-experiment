5fa4c06b2ec8b1f5e99a2f703bf2bc8759c20a39
gagan <140066322+devopsgagan@users.noreply.github.com>
2024-01-07 16:37:29 +0530

    Create wordpress-script-ubuntu20.04.sh

---
+++ wordpress-script-ubuntu20.04.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+### What you’ll learn:
+###        How to set up WordPress
+###        How to configure WordPress
+###        How to create first post
+### What you’ll need:
+###        A computer running Ubuntu Server 20.04 LTS
+###        This guide will also show you how to configure a database for WordPress
+
+sudo apt update -y
+sudo apt install apache2 \
+                 ghostscript \
+                 libapache2-mod-php \
+                 mysql-server \
+                 php \
+                 php-bcmath \
+                 php-curl \
+                 php-imagick \
+                 php-intl \
+                 php-json \
+                 php-mbstring \
+                 php-mysql \
+                 php-xml \
+                 php-zip -y
+sudo mkdir -p /srv/www
+sudo chown www-data: /srv/www
+curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+sudo echo "<VirtualHost *:80>
+    DocumentRoot /srv/www/wordpress
+    <Directory /srv/www/wordpress>
+        Options FollowSymLinks
+        AllowOverride Limit Options FileInfo
+        DirectoryIndex index.php
+        Require all granted
+    </Directory>
+    <Directory /srv/www/wordpress/wp-content>
+        Options FollowSymLinks
+        Require all granted
+    </Directory>
+</VirtualHost>" > /etc/apache2/sites-available/wordpress.conf
+sudo a2ensite wordpress
+sudo a2enmod rewrite
+sudo a2dissite 000-default
+sudo service apache2 reload
+sudo mysql -u root -e 'CREATE DATABASE wordpress;'
+sudo mysql -u root -e 'CREATE USER wordpress@localhost IDENTIFIED BY "password";'
+sudo mysql -u root -e 'GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost IDENTIFIED BY "password";'
+sudo mysql -u root -e 'FLUSH PRIVILEGES;'
+sudo service mysql start
+sudo -u www-data cp /srv/www/wordpress/wp-config-sample.php /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/database_name_here/wordpress/' /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/username_here/wordpress/' /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/password_here/password/' /srv/www/wordpress/wp-config.php
